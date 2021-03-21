e2c31a385fab6aa0c9b9357d0d945b2664088e9f
Grzechu <grzegorz.jamrooz@gmail.com>
2021-03-21 17:19:21 +0100

    refactor(Content) refactor ContentBuilder

--- src/ContentBuilder.php
+++ src/ContentBuilder.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace HtmlCreator;
 
 use HtmlCreator\Element as Element;
+use PlainDataTransformer\Transform;
 
 class ContentBuilder implements ContentBuilderInterface
 {
     public function __construct(
         private string $header,
         private string $nav,
-        private string $sections,
+        private string $main,
         private string $footer,
     ) {
     }
@@ -21,9 +22,7 @@ class ContentBuilder implements ContentBuilderInterface
         return <<<HTML
             $this->header
             $this->nav
-            <main>
-                $this->sections
-            </main>
+            $this->main
             $this->footer
         HTML;
     }
@@ -36,12 +35,10 @@ class ContentBuilder implements ContentBuilderInterface
     public static function createFromArray(array $data): self
     {
         return new self(
-            (string) new Element\Header((string) $data['header'] ??= ''),
-            (string) Element\Nav::createFromArray($data['navbar'] ??= []),
-            (string) new ItemsFactory($data['sections'] ??= [], Element\Section::class),
-            (string) new Element\Footer(
-                (string) new Element\Paragraph((string) $data['footer']['text'] ??= ''),
-            ),
+            (string) new Element\Header(Transform::toString($data['header'] ??= '')),
+            (string) Element\Nav::createFromArray(Transform::toArray($data['navbar'] ??= [])),
+            (string) Element\Main::createFromArray(Transform::toArray($data['main'] ??= [])),
+            (string) Element\Footer::createFromArray(Transform::toArray($data['footer'] ??= [])),
         );
     }
 }
